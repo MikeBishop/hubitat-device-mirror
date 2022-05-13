785b906e8c506ca49de12144fc50055ccc217e72
Mike Bishop <mbishop@evequefou.be>
2022-05-13 10:33:59 -0400

    Initial commit

---
+++ filtered-device.groovy
@@ -0,0 +1,112 @@
+/*
+    Filtered Device Mirror
+    Copyright 2022 Mike Bishop,  All Rights Reserved
+*/
+definition (
+    name: "Filtered Device Mirror", namespace: "evequefou", author: "Mike Bishop", description: "Mirror one capability of a complex device",
+    // importUrl: "TBD",
+    category: "My Apps",
+    iconUrl: "",
+    iconX2Url: ""
+)
+
+preferences {
+    page(name: "mainPage")
+}
+
+Map mainPage() {
+    dynamicPage(name: "Main Page", title: "Filtered Device Mirror", install: true, uninstall: true) {
+        section("General") {
+            input "thisName", "text", title: "Name this Mirror Set", submitOnChange: true
+			if(thisName) app.updateLabel("$thisName")
+        }
+        section("Devices") {
+            input "presenceSensors", "capability.presenceSensor", title: "Presence devices to mirror", required: true, multiple: true
+        }
+        section("Settings") {
+            input "debugSpew", "bool", title: "Log debug events", defaultValue: false
+        }
+    }
+}
+
+private createParentDevice() {
+    def parentDevice = getParentDevice()
+    if (!parentDevice) {
+        String dni = "Filtered-" + app.id.toString()
+        debug "creating Filtered Device: ${dni}"
+        def device = addChildDevice("evequefou", "Filtered Devices", dni, null,
+             [name: "Filtered Devices", label: "Filtered Devices", completedSetup: true ])
+    } else {
+        parentDevice.initialize()
+    }
+    return parentDevice
+}
+
+def getParentDevice() {
+   def deviceIdStr = null
+   def device
+   if (state.childDeviceId) {
+      deviceIdStr = state.childDeviceId
+      device = getChildDevice(deviceIdStr)
+   }
+   if (!device) {
+      def devices = getChildDevices()
+      if (devices.size() > 0) {
+          deviceIdStr = getChildDevices().first().getDeviceNetworkId()
+          state.childDeviceId = deviceIdStr
+          device = getChildDevice(deviceIdStr)
+      }
+   }
+   return device
+}
+
+void updated() {
+	unsubscribe()
+	initialize()
+}
+
+void installed() {
+	initialize()
+}
+
+void initialize() {
+    subscribe(presenceSensors, "presence", handler)
+    presenceSensors.each { refresh(it.getDeviceNetworkId()) }
+}
+
+void refresh() {
+    initialize()
+}
+
+void refresh(id) {
+    def root = getParentDevice()
+    if (root) {
+        debug "Refreshing ${id}"
+        def source = presenceSensors.find { it.getDeviceNetworkId() == id }
+        if (source) {
+            def currentState = [
+                        id: id,
+                        name: source.getLabel() ?: source.getName(),
+                        type: "Presence",
+                        presence: source.currentValue("presence")
+                    ]
+            debug "Mirroring ${source} as ${currentState}"
+            root.parse([currentState])
+        }
+        else {
+            log.warn "Sensor ${id} not found!"
+        }
+    }
+
+}
+
+void handler(evt) {
+    debug "Received ${evt}"
+    refresh(evt.device.getDeviceNetworkId())
+}
+
+void debug(String msg) {
+    if( debugSpew ) {
+        log.debug msg
+    }
+}
---
+++ filtered-parent.groovy
@@ -0,0 +1,58 @@
+/*
+    Filtered Device Mirror
+    Copyright 2022 Mike Bishop,  All Rights Reserved
+*/
+
+metadata {
+    definition (
+        name: "Filtered Devices",
+        namespace: "evequefou",
+        author: "Mike Bishop",
+        // importUrl: "https://raw.githubusercontent.com/MikeBishop/hubitat-teslamate/main/hubitat-teslamate.groovy"
+    ) {
+
+    }
+    preferences {
+        capability "Initialize"
+        capability "Refresh"
+    }
+}
+
+void initialize() {}
+
+void refresh() {
+    log.debug "Refreshing"
+    parent.refresh()
+}
+
+void componentRefresh(child) {
+    def childId = child.getDeviceNetworkId().minus("${device.deviceNetworkId}-").minus("-Presence")
+    parent.refresh(childId)
+}
+
+void parse(String description) { log.warn "parse(String description) not implemented" }
+
+void parse(List description) {
+    description.each {
+        log.debug "Processing ${it}"
+        def rootId = device.deviceNetworkId
+        def childId = "${rootId}-${it.id}-${it.type}"
+        def childLabel = "${it.name} ${it.type}"
+        def cd = getChildDevice(childId)
+        if (!cd) {
+            // Child device doesn't exist; need to create it.
+            cd = addChildDevice("hubitat", "Generic Component Presence Sensor", childId, [isComponent: true])
+            log.debug "Creating ${childLabel} (${childId})"
+        }
+        if (cd.getLabel() != childLabel ) {
+            cd.setLabel("${it.name} ${it.type}")
+        }
+        switch(it.type) {
+            case "Presence":
+                cd.parse(
+                    [[name: "presence", value: it.presence, descriptionText:"${it.name} is ${it.presence}"]]
+                )
+            break
+        }
+    }
+}
